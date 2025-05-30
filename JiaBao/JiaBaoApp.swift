a80b60bfcdb67218b4e796868456338e5cf22f0f
PIN YI <880831ian@gmail.com>
2025-05-31 01:49:51 +0800

    Initial Commit

---
+++ JiaBao/JiaBaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiaBaoApp.swift
+//  JiaBao
+//
+//  Created by pin-yi on 2025/5/31.
+//
+
+import SwiftUI
+
+@main
+struct JiaBaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JiaBaoTests/JiaBaoTests.swift
@@ -0,0 +1,17 @@
+//
+//  JiaBaoTests.swift
+//  JiaBaoTests
+//
+//  Created by pin-yi on 2025/5/31.
+//
+
+import Testing
+@testable import JiaBao
+
+struct JiaBaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
